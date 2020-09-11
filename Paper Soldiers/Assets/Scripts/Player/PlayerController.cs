233c1c6fd9649b3bb0c4350870ac457f6deedc26
MilesButler35 <mbutle35@uncc.edu>
2020-09-11 14:17:38 -0400

    Player Controller movement done, test sprites imported
    Movement for player is done.

---
+++ Paper Soldiers/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 movement;
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D playerRb;
+
+    float horizontalMove;
+    float verticalMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.velocity = new Vector2(horizontalMove * moveSpeed, verticalMove * moveSpeed);
+    }
+}
