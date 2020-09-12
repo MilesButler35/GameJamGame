8fea10816317b4ebe70887b0655cc8c3fb360f6e
MilesButler35 <mbutle35@uncc.edu>
2020-09-11 21:06:25 -0400

    Item Grabbing Done

--- Paper Soldiers/Assets/Scripts/Player/PlayerController.cs
+++ Paper Soldiers/Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,12 @@ public class PlayerController : MonoBehaviour
 
     float horizontalMove;
     float verticalMove;
+
+    public Transform carryLocation;
+    Transform currentItem = null;
+
+    private bool touchingWeapon;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +28,68 @@ public class PlayerController : MonoBehaviour
     {
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
+
+        PickupWeapon();
     }
 
     private void FixedUpdate()
     {
-        playerRb.velocity = new Vector2(horizontalMove * moveSpeed, verticalMove * moveSpeed);
+        playerRb.velocity = new Vector2(horizontalMove * moveSpeed * Time.deltaTime, verticalMove * moveSpeed * Time.deltaTime);
     }
+
+    //Check if the object the player is colliding with is a weapon
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Pickup an item if it has the right tag and the player isn't holding anything else
+        
+            if ((other.CompareTag("Sword") || other.CompareTag("Bow") || other.CompareTag("WizardHat")) && currentItem == null)
+            {
+
+                     Debug.Log("Impact with Weapon");
+                    //Take a reference to Collided Object
+                    currentItem = other.transform;
+                    touchingWeapon = true;
+                
+            }
+        
+    }
+    
+    //Check if the player is leaving the vicinity of a weapon or unit
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if ((other.CompareTag("Sword") && currentItem == null || other.CompareTag("Bow") || other.CompareTag("WizardHat")) && currentItem == null)
+        {
+
+            Debug.Log("Leaving Weapon");
+            
+            currentItem = null;
+            touchingWeapon = false;
+
+        }
+    }
+
+    //Lets the player pick up a weapon if touchingWeapon is true
+    void PickupWeapon()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && touchingWeapon == true)
+        {
+            Debug.Log("Pickup Weapon");
+            //Move it to carrying point 
+            currentItem.position = carryLocation.position;
+
+            //Make it a child of the player so that it moves along with the player
+            currentItem.parent = transform;
+        }
+    }
+    void SetColor()
+    {
+
+    }
+
+    void GiveWeapon()
+    {
+
+    }
+
+
 }
