fa79582e08f9f20d3d5ee4e3d6c36b4caabd0c01
Ruhan Bello <ruhancvbello@gmail.com>
2020-09-12 14:50:56 -0300

    Restarting game when factory is destroyed

---
+++ Paper Soldiers/Assets/Scripts/Managers/FactoryManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FactoryManager : MonoBehaviour
+{
+  private void OnDestroy()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+  }
+}
