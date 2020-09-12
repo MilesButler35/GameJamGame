3bb9aa2204ba43c02be469c2910560b40dd3256b
Ruhan Bello <ruhancvbello@gmail.com>
2020-09-11 23:07:18 -0300

    Slow debufff done

---
+++ Paper Soldiers/Assets/Scripts/Misc/SlowDebuff.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDebuff : MonoBehaviour
+{
+  public int MaxStacksCount;
+  
+  public float IntervalToDeapply = 1;
+  private float _deapplyTimer;
+
+  private readonly float SPEED_MODIFIER = .25f;
+
+  private EntityData _entityData;
+  private int _stacksCount;
+
+  void Start()
+  {
+    _entityData = GetComponent<EntityData>();
+  }
+
+  private void Update()
+  {
+    if (_stacksCount <= 0)
+      return;
+
+    if(_deapplyTimer > 0)
+    {
+      _deapplyTimer -= Time.deltaTime;
+      
+      if(_deapplyTimer <= 0)
+      {
+        Deapply();
+        _deapplyTimer = IntervalToDeapply;
+      }
+    }
+  }
+
+  public void Apply(int stacksAmount)
+  {
+    _stacksCount = Mathf.Max(_stacksCount + 1, MaxStacksCount);
+    _entityData.MovementSpeed = _entityData.MovementSpeed - _stacksCount * SPEED_MODIFIER;
+  }
+
+  public void Deapply()
+  {
+    _stacksCount = Mathf.Max(_stacksCount - 1, 0);
+    _entityData.MovementSpeed = _entityData.MovementSpeed - _stacksCount * SPEED_MODIFIER;
+  }
+}
