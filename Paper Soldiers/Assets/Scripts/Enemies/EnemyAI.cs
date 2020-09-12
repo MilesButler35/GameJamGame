7c83cacc2b42695190b78226f229332a21f2a2a0
Ruhan Bello <ruhancvbello@gmail.com>
2020-09-12 14:10:54 -0300

    Enemies detect and attack Paper Soldiers

--- Paper Soldiers/Assets/Scripts/Enemies/EnemyAI.cs
+++ Paper Soldiers/Assets/Scripts/Enemies/EnemyAI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PaperSoldierAI : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
   public float AttackRange = 1;
   public float AttackInterval = 1;
@@ -23,24 +23,18 @@ public class PaperSoldierAI : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    if(_attackTimer > 0)
+    if (_attackTimer > 0)
     {
       _attackTimer -= Time.deltaTime;
     }
 
     float distanceToTarget = float.MaxValue;
-    Transform targetTransform =  FindClosesTarget(out distanceToTarget);
-    if (targetTransform == null)
-    {
-      _rigidbody2D.velocity = Vector2.zero;
-      return;
-    }
-
-    if (distanceToTarget < AttackRange)
+    Transform targetTransform = FindClosesTarget(out distanceToTarget);
+    if (targetTransform != null)
     {
       _rigidbody2D.velocity = Vector2.zero;
 
-      if(_attackTimer <= 0)
+      if (_attackTimer <= 0)
       {
         _entityAttack.Attack(targetTransform.position);
         _attackTimer = AttackInterval;
@@ -48,21 +42,26 @@ public class PaperSoldierAI : MonoBehaviour
 
       return;
     }
-    
-    Vector2 movementDirection = (targetTransform.position - transform.position).normalized;
-    _rigidbody2D.velocity = movementDirection * _entityData.MovementSpeed;
+
+    _rigidbody2D.velocity = -transform.right * _entityData.MovementSpeed;
   }
 
   private Transform FindClosesTarget(out float distanceToTarget)
   {
-    GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+    Collider2D[] closeEnemies = Physics2D.OverlapCircleAll(transform.position, AttackRange);
 
     Transform targetTransform = null;
     float minDistance = float.MaxValue;
-    foreach (var enemy in allEnemies)
+    foreach (var enemy in closeEnemies)
     {
+      if (enemy.gameObject == this.gameObject)
+        continue;
+
+      if (enemy.tag != "PaperSoldier")
+        continue;
+
       float dist = Vector2.Distance(transform.position, enemy.transform.position);
-      if(dist < minDistance)
+      if (dist < minDistance)
       {
         minDistance = dist;
         targetTransform = enemy.transform;
