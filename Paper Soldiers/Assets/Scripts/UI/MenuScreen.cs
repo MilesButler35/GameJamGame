6e86107602e4c52924438dc843b571d37fd09b81
Ruhan Bello <ruhancvbello@gmail.com>
2020-09-13 18:42:34 -0300

    Tutorial done

--- Paper Soldiers/Assets/Scripts/UI/MenuScreen.cs
+++ Paper Soldiers/Assets/Scripts/UI/MenuScreen.cs
@@ -3,6 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuScreen : MonoBehaviour
 {
+  public GameObject TutorialScreen;
+  public GameObject MainScreen;
+
+  private void Update()
+  {
+    if (Input.anyKeyDown == true && MainScreen.activeSelf == false && TutorialScreen.activeSelf == true)
+    {
+      MainScreen.SetActive(true);
+      TutorialScreen.SetActive(false);
+    }
+  }
+
   public void StartGame()
   {
     SceneManager.LoadScene("GameScene");
@@ -10,6 +22,8 @@ public class MenuScreen : MonoBehaviour
 
   public void Tutorial()
   {
+    MainScreen.SetActive(false);
+    TutorialScreen.SetActive(true);
   }
 
   public void Quit()
